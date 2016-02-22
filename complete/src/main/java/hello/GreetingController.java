6a84b995e878b2abcfe89f9a8748d5eecf436d07
ror <priya.siddana@gmail.com>
2016-02-22 15:09:38 +0530

    gs-rest-service

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -4,16 +4,31 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
 
 @RestController
 public class GreetingController {
-
-    private static final String template = "Hello, %s!";
+    @RequestMapping("/greeting")
+     public ArrayList greeting(){
+      Greeting g1 = new Greeting(1,"project");
+      Greeting g2 = new Greeting(2,"work");
+      Greeting g3 = new Greeting(3,"hiii");
+      ArrayList al=new ArrayList();
+      al.add(g1);
+      al.add(g2);
+      al.add(g3);
+      return al;
+      
+   /* private static final Object[] template =[{id:1},{id:2}];
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    public Object greeting(@RequestParam(value="name", defaultValue="priya") String name) {
+        Greeting result = new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));*/
+      
+           
+                        
+                     
     }
 }
